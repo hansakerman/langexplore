fea3a7fecdd90da771a029e88081da50f1629c97
Heinz <113355864+heinzfieldmann@users.noreply.github.com>
2024-12-17 20:01:34 +0100

    mathstuff

---
+++ c++/maffs.cpp
@@ -0,0 +1,21 @@
+#include <cmath>
+#include <iostream>
+using namespace std;
+
+// MAFFS!
+
+int main(){
+  cout <<min(1,5)<<endl;
+  cout <<max(1,5)<<endl;
+  cout <<sqrt(13*13)<<endl;
+  cout <<round(42.17)<<endl;
+  cout << log(2)<<endl;
+
+  bool nope=false;
+  bool yup=true;
+  cout << yup << endl;
+  cout << nope << endl;
+  cout << !(nope==(1==2)) << endl;
+  
+}
+
