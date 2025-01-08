0ca3d7539dde154360cf428ef124046b786f1e0f
Hans Åkerman <hans.akerman@gmail.com>
2025-01-08 18:57:47 +0100

    algo_c++

--- c++/iterators.cpp
+++ c++/iterators.cpp
@@ -8,9 +8,24 @@ int main (){
 
   vector<string> cars = {"Volvo","BMW","Ford","Mazda"};
   //  vector<string>::iterator it;
-  auto it = cars.begin();
-  for (it;it !=cars.end();++it){
-    cout << *it <<endl;
-  }
-  // I am here: For-Each Loop vs. Iterators  
+  //auto it = cars.begin();
+  //for (it;it !=cars.end();++it){
+  //  cout << *it <<endl;
+  //}
+  // I am here: For-Each Loop vs. Iterators
+  // If we want to mess with items then you might have to use an iterator
+
+  for(auto it = cars.begin();it != cars.end();){
+    if (*it == "BMW"){
+      it=cars.erase(it);}else{++it;}
+  } 
+
+  for (const string& car : cars) {
+    cout << car << endl;}
+  auto foo=cars.begin();
+    auto foo2=cars.rend();
+    cout << &foo<<endl;
+    cout << &foo2<<endl;
+
 }
+
---
+++ glosa/README.md
@@ -0,0 +1,13 @@
+
+Gör det enklaste först.
+
+Ha en fil med "svåra ord".
+
+<engelska ordet> <svenska ord>
+
+Första versionen.
+läs in glosfilen.txt
+Ta ett random ord och skriv ut det på skärmen.
+
+
+Framöver:
