e7bf3b8a8b425f7c189b1169f02804d46a67c40f
Hans Åkerman <113355864+heinzfieldmann@users.noreply.github.com>
2025-01-11 16:02:52 +0100

    Course start

---
+++ The Beginner's guide to Advanced C programming for Linux/name.c
@@ -0,0 +1,13 @@
+/* name.c  */
+#include <stdio.h>
+
+int main()
+{
+
+  char name[32];
+
+  printf("What is your name\n");
+  scanf("%s", &name);
+  
+  
+}
---
+++ The Beginner's guide to Advanced C programming for Linux/numbers.c
@@ -0,0 +1,5 @@
+/* numbers.c */
+#include <stdio.h>
+
+int main()
+{}
