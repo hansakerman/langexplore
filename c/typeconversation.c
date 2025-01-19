ca74c6fe664d6e09d37aed56351703802c5c0272
Hans Åkerman <hans.akerman@gmail.com>
2025-01-19 02:17:59 +0100

    c

---
+++ c/typeconversation.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+
+  // this will be 2 
+  float sum = 5 / 2;
+  printf("nah %f\n", sum);
+
+  // this will be correct 2.5
+  float sum2 = (float) 5 / 2;
+  printf("yeah %f\n", sum2);
+  
+}
